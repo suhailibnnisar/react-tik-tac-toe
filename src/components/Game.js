849beb4381019f0ca4afbf93885dadf2cc9ca865
Suhail Ibn Nisar <suhailibnnisar@gmail.com>
2022-05-26 14:00:44 +0545

    feat: time travel

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,38 +8,46 @@ const style = {
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(false);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (index) => {
-    const newBoard = [...board];
-    if (!newBoard[index] && !winner) {
-      newBoard[index] = xIsNext ? 'O' : 'X';
-      setXIsNext(!xIsNext);
-      setBoard(newBoard);
-    }
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+
+    if (winner || squares[index]) return;
+
+    squares[index] = xIsNext ? 'O' : 'X';
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
+    setXIsNext(!xIsNext);
   };
 
-  const jumpTo = () => {};
-
-  const renderMoves = () => {};
-
-  const restartGame = () => {
-    setBoard(Array(9).fill(null));
-    setXIsNext(false);
+  const jumpTo = (index) => {
+    setStepNumber(index);
+    setXIsNext(index % 2 === 0);
   };
 
   return (
     <Fragment>
-      <Board squares={board} handleClick={handleClick} />
+      <Board squares={history[stepNumber]} handleClick={handleClick} />
       <div style={style}>
         <p>
           {winner
             ? 'Winner: ' + winner
             : 'Next Player: ' + (xIsNext ? 'O' : 'X')}
         </p>
-        <button onClick={restartGame}>Restart</button>
+        {history.map((_, index) => {
+          const destination = index ? `Go to move #${index}` : 'Restart';
+          return (
+            <li>
+              <button onClick={() => jumpTo(index)}>{destination}</button>
+            </li>
+          );
+        })}
       </div>
     </Fragment>
   );
